ff04fa8d2b0486db46f3a2bbb2b5293a9d3f61c8
Bernardo Zamin <126703680+Bernardo-Zamin@users.noreply.github.com>
2023-08-10 08:10:21 -0300

    Commit Inicial

---
+++ T1/src/App.java
@@ -0,0 +1,50 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String dna = "DANNDANDANDANADNDDDAN";
+
+        StringBuilder sb = new StringBuilder(dna);
+
+        System.out.println("O dna antes do algoritmo: " + sb.toString());
+        System.out.println();
+
+        int index = 0; // posicao 0 da string
+
+        while (index < sb.length() - 1) {
+            char caracterAtual = sb.charAt(index);
+            char proximoCaracter = sb.charAt(index + 1);
+            int inicio = 0;
+            int fim = 1;
+
+            if (caracterAtual != proximoCaracter) {
+                if (caracterAtual == 'D' && proximoCaracter == 'A' || caracterAtual == 'A' && proximoCaracter == 'D') {
+                    sb.delete(inicio, fim);
+                    inicio += 1;
+                    fim += 1;
+                    sb.insert(sb.length() - 1, 'N');
+                }
+                if (caracterAtual == 'N' && proximoCaracter == 'D' || caracterAtual == 'D' && proximoCaracter == 'N') {
+                    sb.delete(inicio, fim);
+                    inicio += 1;
+                    fim += 1;
+                    sb.insert(sb.length() - 1, 'A');
+                }
+                if (caracterAtual == 'N' && proximoCaracter == 'A' || caracterAtual == 'A' && proximoCaracter == 'N') {
+                    sb.delete(inicio, fim);
+                    inicio += 1;
+                    fim += 1;
+                    sb.insert(sb.length() - 1, 'D');
+                }
+            } else {
+                index += 2;
+
+            }
+
+        }
+
+        String dnaFinal = sb.toString();
+
+        System.out.println(dnaFinal);
+
+    }
+}
