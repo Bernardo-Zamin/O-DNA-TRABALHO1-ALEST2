175198dbdf6445a7777a6fbef245a5a7ac9b5e82
Bernardo-Zamin <bezamin03@gmail.com>
2023-09-13 15:39:31 -0300

    adicionado contagem de tempo

--- T1/src/App.java
+++ T1/src/App.java
@@ -4,42 +4,45 @@ public class App {
     public static void main(String[] args) throws Exception {
         ArrayList<Character> lista = new ArrayList<>();
         Characters chars = new Characters();
-        LeituraArquivo leArq = new LeituraArquivo("caso10k.txt");
+        LeituraArquivo leArq = new LeituraArquivo("caso100k.txt");
 
+        String str = leArq.carregaDados();
 
-
-        String listaDna = leArq.carregaDados();
-
-        for (int j = 0; j < listaDna.length(); j++) {
-            lista.add(listaDna.charAt(j));
+        for (int i = 0; i < str.length(); i++) {
+            lista.add(str.charAt(i));
         }
 
         System.out.println("\n=========================================================");
         System.out.println("            TAMANHO INICIAL: " + lista.size() + "               ");
         System.out.println("=========================================================\n");
 
-        int l = 0;
-        int k = l + 1;
+        int i;
+        int j;
         char novo;
-        
-        
 
-        for (l = 0; l < lista.size() - 1; l++) {
-            k = l + 1;
-            if (chars.precisaMudar(lista.get(l), lista.get(k))) {
-                novo = chars.letraNoFim(lista.get(l), lista.get(k));
-                lista.remove(l);
-                lista.remove(l);
+        long inicio = System.currentTimeMillis();
+
+        for (i = 0; i < lista.size() - 1; i++) {
+
+            j = i + 1;
+
+            if (chars.precisaMudar(lista.get(i), lista.get(j))) {
+
+                novo = chars.letraNoFim(lista.get(i), lista.get(j));
+                lista.remove(i);
+                lista.remove(i);
                 lista.add(novo);
 
-                l = -1;
-                k = 0;
+                i =-1;
+                j = 0;
 
             }
 
         }
 
-        System.out.println("TAMANHO FINAL: " + lista.size());
+        long fim = System.currentTimeMillis();
+        long segundos = (fim - inicio) / 1000;
+        System.out.println("TAMANHO FINAL: " + lista.size() + "\n" + segundos + " SEGUNDOS");
         System.out.println(lista);
 
     }
--- T1/src/LeituraArquivo.java
+++ T1/src/LeituraArquivo.java
@@ -1,8 +1,5 @@
 import java.io.BufferedReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.FileReader;
 
 public class LeituraArquivo {
     private String nArq;
@@ -11,25 +8,12 @@ public class LeituraArquivo {
         this.nArq = nomeArquivo;
 
     }
-    // Método base foi fornecido por Bernardo Copstein para realizar trabalho de
-    // Fundamentos de Desenvolvimento de Software:
-    // https://github.com/TP-BCopsteinAssignments/ExercicioPrimeirosPadroes/blob/main/src/Consultas.java
-    // O método foi adaptado para uma classe de leitura de arquivo:
-    // https://github.com/Bernardo-Zamin/T1-Fundamentos-de-D.-de-Software/blob/main/src/LeArquivo.java
-    // Método também adaptado para classe e projeto atual
-    // Ajuda na organização do código
 
     public String carregaDados() {
-        String currDir = Paths.get("").toAbsolutePath().toString();
-        // Monta o nome do arquivo
-        String nomeCompleto = currDir + "/" + nArq;
-        System.out.println(nomeCompleto);
-        // Cria acesso ao "diretorio" da mídia (disco)
-        Path path = Paths.get(nomeCompleto);
 
         String linha = "";
 
-        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(nArq))) {
 
             linha = reader.readLine();
 
